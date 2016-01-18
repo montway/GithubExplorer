a8f142d3cdd7dbbed9af1af694139d0ea31bd5b9
Ryan Yogan <ryanyogan@gmail.com>
2016-01-18 14:08:19 -0600

    added little usage helper

--- app/ios_bridges/Encoding.m
+++ app/ios_bridges/Encoding.m
@@ -7,6 +7,10 @@ RCT_EXPORT_MODULE();
 
 /**
  * @params base64Encode [NSString]
+ *
+ * Usage: import { Encoding } from 'NativeModules'; (es2015)
+ * Usage: var encoding = require('NativeModules').Encoding; (es5)
+ *
  */
 RCT_EXPORT_METHOD(base64Encode:(NSString *)str callback:(RCTResponseSenderBlock)callback)
 {
